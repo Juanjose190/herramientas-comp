4d36a42779caab0960fff65aae3e8f7c7deb9ec1
Juan José <86538701+Juanjose190@users.noreply.github.com>
2025-10-20 10:03:49 -0500

    Update bucles.py

--- bucles.py
+++ bucles.py
@@ -1,6 +1,21 @@
+#Juan José Burbano - Ingeniería de software 
+
 
 for i in range (11, 0, -1):
     if(i % 2 == 0):
         print(f"{i} no tiene residuo (es par)")
     else:
         print(f"{i} tiene residuo (es impar)")
+
+
+
+
+#ejercicio con bucle while
+#i = 11; 
+
+#while i > 0:
+    #if(i % 2 == 0):
+        #print(f"{i} no tiene residuo, es par")
+    #else:
+       # print(f"{i} tiene residuo, es impar")
+    #i -= 1
